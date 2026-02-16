19cc2fc38713c1e51d3ebe23c26e6933f740b7f5
hulmona <ishanda2010@gmail.com>
2026-02-16 07:39:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -85,7 +85,7 @@ auth_channels     = environ.get("AUTH_CHANNELS", "-100")# Channels for force sub
 QR_CODE = environ.get('QR_CODE', 'Your_Qr_Code')    # QR code image for payments
 OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'ɴᴏ ᴀᴠᴀɪʟᴀʙʟᴇ ʀɪɢʜᴛ ɴᴏᴡ')    # Owner UPI ID for payments
 
-STAR_PREMIUM_PLANS = {
+STAR_PREMIUM_PLANS = {}
 ION = bool(environ.get('MOVIE_UPDATE_NOTIFICATION', False))  # Notification On (True) / Off (False)
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-100'))  # Notification of sent to your channel
 DREAMXBOTZ_IMAGE_FETCH = bool(environ.get('DREAMXBOTZ_IMAGE_FETCH', True))  # On (True) / Off (False)
