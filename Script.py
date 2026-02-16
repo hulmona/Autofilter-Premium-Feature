0e83d72753e20d4494bd0c404cb908e2c8e1da74
hulmona <ishanda2010@gmail.com>
2026-02-16 12:37:46 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -230,20 +230,17 @@ Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     MOVIE_UPDATE_NOTIFY_TXT = """
 </b><a href={poster_url}>📥</a><a href={imdb_url}>New {tag} Added</a></b>
 
-<blockquote>✨ ᴛɪᴛʟᴇ : <code>{filename}</code>
+<blockquote>✨ TITLE : <code>{filename}</code>
 
-
-🎭 ɢᴇɴʀᴇs : <b>{genres}</b>
-📺 ᴏᴛᴛ        : <b>{ott}</b>
-🎞️ ǫᴜᴀʟɪᴛʏ : <b>{quality}</b>
-🎧 ᴀᴜᴅɪᴏ    : <b>{language}</b>
-🔥 ʀᴀᴛɪɴɢ   : <b>{rating}</b>
+🎭 GENRES : <b>{genres}</b>
+📺 OTT : <b>{ott}</b>
+🎞️ QUALITY : <b>{quality}</b>
+🎧 AUDIO : <b>{language}</b>
+🔥 RATING : <b>{rating}</b>
 {episodes}
 </blockquote>
 
-
-🔍 <b>Sᴇᴀʀᴄʜ →</b> {search_link}
-"""
+🔎 <b>Search
 
 
     IMDB_TEMPLATE_TXT = """<b><a href={url}>{title} (<a href={url}/releaseinfo>{year}</a>)
